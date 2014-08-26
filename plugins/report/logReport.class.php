fe0f07bc9d049fe1800a8a22e9d6a3fac2bce2ec
Gildas NOEL <info@ackwa.fr>
2014-08-26 09:01:52 +0200

    Correct invalid _log() call

--- plugins/report/logReport.class.php
+++ plugins/report/logReport.class.php
@@ -7,7 +7,7 @@
 class logReport extends maliciousReport {
     function report($lChecks) {
         foreach($lChecks as $oO) {
-            _log("%-20s : %4d / %4d\n", get_class($oO), count($oO->lFiles), $oO->iCount);
+            _log(sprintf("%-20s : %4d / %4d", get_class($oO), count($oO->lFiles), $oO->iCount));
             if (count($oO->lFiles) && $oO->warm()) _log(print_r($oO->lFiles, true));
         }
     }
