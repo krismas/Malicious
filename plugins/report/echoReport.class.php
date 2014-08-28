ba5cf64422cbdda1513427f44a2424bd37ea3b2a
Gildas NOEL <info@ackwa.fr>
2014-08-28 19:12:57 +0200

    Add call to description() method

--- plugins/report/echoReport.class.php
+++ plugins/report/echoReport.class.php
@@ -8,7 +8,7 @@ class echoReport extends maliciousReport {
     function report($lChecks) {
         if (isset($_SERVER['REMOTE_ADDR'])) echo '<pre>';
         foreach($lChecks as $oO) {
-            printf("%-20s : %4d / %4d\n", get_class($oO), count($oO->lFiles), $oO->iCount);
+            printf("%-60s [%-12s] : %4d / %4d\n", $oO->description(), str_replace('Check', '', get_class($oO)), count($oO->lFiles), $oO->iCount);
             if (count($oO->lFiles) && $oO->warn()) print_r($oO->lFiles);
         }
         if (isset($_SERVER['REMOTE_ADDR'])) echo '</pre>';
