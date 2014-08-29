e9cbdf9eed2d8446c6661f92682d20fd05a40cf8
Gildas NOEL <info@ackwa.fr>
2014-08-29 07:31:54 +0200

    Display bytes read

--- plugins/report/logReport.class.php
+++ plugins/report/logReport.class.php
@@ -7,7 +7,7 @@
 class logReport extends maliciousReport {
     function report($lChecks) {
         foreach($lChecks as $oO) {
-            _log(sprintf("%-60s [%-12s] : %4d / %4d", $oO->description(), str_replace('Check', '', get_class($oO)), count($oO->lFiles), $oO->iCount));
+            _log(sprintf("%-60s [%-12s] : %4d / %4d files - %8d bytes read", $oO->description(), str_replace('Check', '', get_class($oO)), count($oO->lFiles), $oO->iCount, $oO->iSize));
             if (count($oO->lFiles) && $oO->warn()) _log(print_r($oO->lFiles, true));
         }
     }
