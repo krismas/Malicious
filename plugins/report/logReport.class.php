d565731c6497037f8f70b3188dcc480301194a7c
Gildas NOEL <info@ackwa.fr>
2014-09-17 13:06:53 +0200

    Replace print_r()

--- plugins/report/logReport.class.php
+++ plugins/report/logReport.class.php
@@ -7,10 +7,10 @@
 class logReport extends maliciousReport {
     function report($lChecks) {
         foreach($lChecks as $oO) {
-            _log(sprintf("%-60s [%-12s] : %4d / %4d files - %8d bytes read", $oO->description(), str_replace('Check', '', get_class($oO)), count($oO->lFiles), $oO->iCount, $oO->iSize));
+            _log(sprintf("%-70s [%-12s] : %4d / %4d files - %8d bytes read", $oO->description(), str_replace('Check', '', get_class($oO)), count($oO->lFiles), $oO->iCount, $oO->iSize));
             if (count($oO->lFiles) && $oO->warn()) {
                 arsort($oO->lFiles);
-                _log(print_r($oO->lFiles, true));
+                foreach($oO->lFiles as $k =>$v) _log(sprintf(" %-12s|%20s : %s", str_replace('Check', '', get_class($oO)), $v, $k));
             }
         }
     }
